9319ed702f4f59adbe77a9ca72e3253124e62f06
Justin Lin <ro0566320@gmail.com>
2020-03-31 12:00:06 -0400

    finish the diningroom.js

---
+++ models/diningroom.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const diningroomSchema = new Schema({
+  seats: { type: Number, required: true },
+  twoSeat: { type: Boolean, default: false },
+  fourSeat: { type: Boolean, default: false },
+  sixSeat: { type: Boolean, default: false },
+  availability: { type: Boolean, default: true}
+});
+
+const Diningroom = mongoose.model("Diningroom", diningroomSchema);
+
+module.exports = Diningroom;
+
+
