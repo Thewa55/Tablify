04eb990b4f26153d8f983a62341229d58e1cb354
Justin Lin <ro0566320@gmail.com>
2020-03-31 13:11:01 -0400

    finish tableHistory.js and the entire models are ready

---
+++ models/tableHistory.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const tableHistorySchema = new Schema({
+    start_at: { type: Number },
+    end_at: { type: Number },
+    table_color: { type: String, default: 'White'},
+    order: { type: Text },
+    order_quantity: { type: String }
+});
+
+const TableHistory = mongoose.model("Book", tableHistorySchema);
+
+module.exports = TableHistory;
+
