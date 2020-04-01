0fa9f5d99e1b394da08a9a2e25f58fc276f1b1a4
Anamika Panna <anamika2028@gmail.com>
2020-04-01 10:38:54 -0400

    QR initial code

---
+++ QR_code_develpment/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Section from "./components/Section";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Section />
+    </div>
+  );
+}
+
+export default App;
