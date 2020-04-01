9f7d867565673cf7271d8c392e551d8718d562d1
kevin-salamon <kevinwebdevs@gmail.com>
2020-04-01 17:45:51 -0400

    added basic templates for new tables

---
+++ client/src/components/TableExamples/Large.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './tablestyle.css';
+
+function Large() {
+    return (
+        <div className="table-large text-center">Test Large</div>
+    );
+};
+
+export default Large;
---
+++ client/src/components/TableExamples/XL.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './tablestyle.css';
+
+function XL() {
+    return (
+        <div className="table-xl text-center">Test XL</div>
+    );
+};
+
+export default XL;
