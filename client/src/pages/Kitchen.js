5af93e735b990b7f43d841da0697d3496172335d
Anamika Panna <anamika2028@gmail.com>
2020-04-01 14:07:59 -0400

    adding card layout for tables

--- client/src/pages/Kitchen.js
+++ client/src/pages/Kitchen.js
@@ -4,8 +4,20 @@ import { Link } from "react-router-dom";
 
 function Kitchen() {
   return (
-    <div>
-        test
+    <div>Test
+        <div class="col-md-4">
+        <div class="card mt-2">
+          <div class="card-body">
+            <h5 class="card-title">Table1</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Appetizer 1</h6>
+            <h6 class="card-subtitle mb-2 text-muted">Appetizer 2</h6>
+            <h6 class="card-subtitle mb-2 text-muted">Entre 1</h6>
+            <h6 class="card-subtitle mb-2 text-muted">Entre 2</h6>
+            <button onclick="Prepare()">Prepare</button>
+            <button onclick="Done()">Done</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
