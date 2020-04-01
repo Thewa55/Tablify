f59ede5cbd604a8f0b160a8eb61d6c2f40ab63c3
Justin Lin <ro0566320@gmail.com>
2020-04-01 10:06:07 -0400

    finish the relationship between two schemas tableHistory and diningroom

--- models/diningroom.js
+++ models/diningroom.js
@@ -1,12 +1,21 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const tableHistorySchema = new Schema({
+  start_at: { type: Number },
+  end_at: { type: Number },
+  table_color: { type: String, default: 'White'},
+  order: { type: Text },
+  order_quantity: { type: String }
+});
+
 const diningroomSchema = new Schema({
   seats: { type: Number, required: true },
   twoSeat: { type: Boolean, default: false },
   fourSeat: { type: Boolean, default: false },
   sixSeat: { type: Boolean, default: false },
-  availability: { type: Boolean, default: true}
+  availability: { type: Boolean, default: true},
+  tableHistory: [tableHistorySchema]
 });
 
 const Diningroom = mongoose.model("Diningroom", diningroomSchema);
