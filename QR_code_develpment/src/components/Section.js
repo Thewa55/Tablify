201a96de61b76d0104b3bc90ddabd2576e39b834
Anamika Panna <anamika2028@gmail.com>
2020-04-02 11:00:18 -0400

    qr code working function

--- QR_code_develpment/src/components/Section.js
+++ QR_code_develpment/src/components/Section.js
@@ -1,15 +1,6 @@
 import React from "react";
 import "../styles/Section.css";
-// var QRCode = require('react-qr');
-// import QRCode from "react-qr";
 import QRCode from "qrcode.react";
-
-// By importing the Section.css file, it is added to the DOM whenever this component loads
-
-// We can also style a component inside of its JavaScript file by adding style properties to its rendered elements
-// Unlike regular HTML, a JSX style property must be an object instead of a string
-// On a style object, we camelCase all property names, and put all of the values in quotes
-// Non quoted values default to "pixels", e.g. height, margin, padding
 
 const styles = {
   sectionStyles: {
@@ -17,33 +8,24 @@ const styles = {
   }
 };
 
-// We use JSX curly braces to evaluate the style object on the JSX tag
-
 class Section extends React.Component {
   state = {
-isQrCode: false,
-qrValue: ""
-
+    isQrCode: false,
+    qrValue: ""
   };
 
-
   generateQr = () => {
-     this.setState({isQrCode: true, qrValue: "youtube.com"})
-  }
+    this.setState({ isQrCode: true, qrValue: "youtube.com" });
+  };
   render() {
     return (
       <section style={styles.sectionStyles} className="section">
         <p>Click me to generate QR Code!</p>
-        {/* <QRCode text='1HB5XMLmzFVj8ALj6mfBsbifRoD4miY36v'/> */}
-
-        {
-          this.state.isQrCode ? 
-          <QRCode value={this.state.qrValue} />:
-          <></>
-        }
-        
+        {this.state.isQrCode ? <QRCode value={this.state.qrValue} /> : <></>}
         <div id="response"></div>
-        <button onClick={this.generateQr} id="start">Generate</button>
+        <button onClick={this.generateQr} id="start">
+          Generate
+        </button>
       </section>
     );
   }
