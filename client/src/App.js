3254e58eb3667fd5cce0ba8fd68f5dfe7ce6df5a
Kenny Lam <kinwai.lam730@gmail.com>
2020-04-02 15:36:38 -0400

    Did some formating for app.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,16 +9,16 @@ import PasswordForgetPage from './components/PasswordForget';
 import { withAuthentication } from './components/Session';
 
 const App = () => (
-        <Router>
-          <Fragment>
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/FrontDesk" component={FrontDesk} />
-            <Route exact path="/SignIn" component={SignIn} />
-            <Route exact path="/pw-forget" component={PasswordForgetPage} />
-            <Route exact path="/SignUp" component={SignUp} />
-            <Route exact path="/Kitchen" component={Kitchen} />
-          </Fragment>
-        </Router>
+  <Router>
+    <Fragment>
+      <Route exact path="/" component={Landing} />
+      <Route exact path="/FrontDesk" component={FrontDesk} />
+      <Route exact path="/SignIn" component={SignIn} />
+      <Route exact path="/pw-forget" component={PasswordForgetPage} />
+      <Route exact path="/SignUp" component={SignUp} />
+      <Route exact path="/Kitchen" component={Kitchen} />
+    </Fragment>
+  </Router>
 )
 
 export default withAuthentication(App);
