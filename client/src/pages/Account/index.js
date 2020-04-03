e21524d5c6366def6d282c1e3f134ee763a01975
Kenny Lam <kinwai.lam730@gmail.com>
2020-04-03 18:14:14 -0400

    Adding employee page

--- client/src/pages/Account/index.js
+++ client/src/pages/Account/index.js
@@ -1,25 +1,29 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import { AuthUserContext, withAuthorization } from '../../components/Session';
 import PasswordChangeForm from '../../components/PasswordChange';
 import Jumbotron from '../../components/Jumbotron'
 import Container from '../../components/Container'
+
 const AccountPage = () => (
   <AuthUserContext.Consumer>
     {authUser => (
       <div>
         <Container>
-      <Jumbotron>
-        <div className="Row">
-          <div className="col-md-4 offset-md-4">
-
-        <h1>Account: {authUser.email}</h1>
-        <br />
-        <PasswordChangeForm />
-
-        </div>
-        </div>
-      </Jumbotron>
-    </Container>
+          <Jumbotron>
+            <div className="Row">
+              <div className="col-md-4 offset-md-4">
+                <h1>Account: {authUser.email}</h1>
+                <br />
+                <PasswordChangeForm />
+                <br />
+                <Link to="/Employees">
+                  <button className="login-button">Employees</button>
+                </Link>
+              </div>
+            </div>
+          </Jumbotron>
+        </Container>
       </div>
     )}
   </AuthUserContext.Consumer>
