a3a85624e90548e4d3d39bb87deb47aed7b569c6
Kenny Lam <kinwai.lam730@gmail.com>
2020-04-05 00:51:46 -0400

    Added modal for updating employees

--- client/src/components/EmployeeList/index.js
+++ client/src/components/EmployeeList/index.js
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from "react";
 import API from "../../utils/API";
 import EmployeeModal from "../EmployeeModal"
 import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
 
 function EmployeeList() {
-    
+  const [show, setShow] = useState(false); 
   const [employees, setEmployees] = useState([])
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   function getEmployees() {
     API.getEmployees()      
@@ -42,7 +46,26 @@ function EmployeeList() {
                 <li className="list-group-item">{employee.email}</li>
               </ul>
               <Button variant="danger" onClick={() => removeEmployee(employee._id)}>Remove Employee</Button>{' '}
-            </>
+              <Button variant="primary" className="mt-4" onClick={handleShow}>
+                    Edit {employee.name}
+              </Button>
+                  <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>Add an employee</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      hello
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="secondary" onClick={handleClose}>
+                        Cancel
+                      </Button>
+                      <Button variant="primary" onClick={handleClose}>
+                        Add Employee
+                      </Button>
+                    </Modal.Footer>
+                  </Modal>
+                </>
           ))}
           <div>
             <EmployeeModal />
