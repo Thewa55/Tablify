a0ca891f49aef03984c2932527706c48ed889ae9
Kenny Lam <kinwai.lam730@gmail.com>
2020-04-06 15:04:42 -0400

    Updating revenue code

--- client/src/components/RevenueList/index.js
+++ client/src/components/RevenueList/index.js
@@ -1,22 +1,39 @@
 import React, { useEffect, useState } from "react";
 import API from "../../utils/API";
-import Button from 'react-bootstrap/Button'
-import Table from 'react-bootstrap/Table'
+// import Button from 'react-bootstrap/Button'
+// import Table from 'react-bootstrap/Table'
+import Moment from 'react-moment';
+
 
 function RevenueList(){
   const [tableHistory, setTableHistory] = useState([]);
+  const [today, setToday] = useState()
+  const []
 
   function getTableHistory(){
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today = mm + '/' + dd + '/' + yyyy;
+    setToday(today)
+
     API.getTableHistory()
       .then(results => {
         setTableHistory(results.data)
-      })
-  }
+    })
 
+    
+  };
 
+  useEffect(() => {
+    getTableHistory()
+  }, []);
+
+  console.log(today)
   return(
     <div>
-      This is the Revenue List
+      This is the Revenue List. Today is {today}
     </div>
   )
 }
