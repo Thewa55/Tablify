b8aaf3d69f47c30805d86c83f7e1aa543d2a4a65
Kenny Lam <kinwai.lam730@gmail.com>
2020-04-07 23:37:58 -0400

    Able to generate a table, also fixed missing lines

--- client/src/components/RevenueChart/index.js
+++ client/src/components/RevenueChart/index.js
@@ -1,65 +1,71 @@
-// import React from 'react';
-// import {Line} from 'react-chartjs-2';
-// import moment from 'moment';
-
-// function RevenueChart (){
-//   const today = moment().format('L').toString();
-//   const yesterday = moment().subtract(1, 'day').format('L');
-//   const threeDays = moment().subtract(2,'day').format('L');
-//   const fourDays = moment().subtract(3,'day').format('L');
-//   const fiveDays = moment().subtract(4,'day').format('L')
-//   const sixDays = moment().subtract(5,'day').format('L')
-//   const sevenDays = moment().subtract(6,'day').format('L')
-// }
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
 import {Line} from 'react-chartjs-2';
-import moment from 'moment';
 
 
-const state = {
-  labels: ['January', 'February', 'March',
-           'April', 'May'],
-  datasets: [
-    {
-      label: 'Rainfall',
-      fill: false,
+function RevenueChart(props){
+  console.log("This props: ",props)
+  const [show, setShow] = useState(false); 
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true)
+
+  const [chart, setChart] = useState({
+    labels: [],
+    datasets: [{
+      label: 'Dollars',
+      Fill: false,
       lineTension: 0.5,
       backgroundColor: 'rgba(75,192,192,1)',
       borderColor: 'rgba(0,0,0,1)',
       borderWidth: 2,
-      data: [65, 59, 80, 81, 56]
-    }
-  ]
+      data: []}
+    ]
+  })
+
+  function setPage() {
+    setChart({...chart, labels: props.daysOfWeek, 
+      datasets:[{data: [props.total.today, props.total.yesterday, props.total.threeDays, props.total.fourDays, props.total.fiveDays, props.total.sixDays, props.total.sevenDays]}]
+      // datasets: {data: [props.total.today, props.total.yesterday, props.total.threeDays, props.total.fourDays, props.total.fiveDays, props.total.sixDays, props.total.sevenDays]}
+    })
+    handleShow()
+  }
+  
+  return(
+    <>
+      <button variant="primary" style={{width: "100px", margin: "1%"}} className="btn btn-primary" onClick={setPage}>
+        Chart
+      </button>
+    
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>7-Day Revenue</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+            <Line
+        data={chart}
+        options={{
+          title:{
+            display:true,
+            text:'Weekly Revenue',
+            fontSize:20
+          },
+          legend:{
+            display:true,
+            position:'right'
+          }
+        }}
+      />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Cancel
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </>
+  )
 }
 
-export default class RevenueChart extends React.Component {
-  render() {
-    
-    const today = moment().format('L').toString();
-    const yesterday = moment().subtract(1, 'day').format('L');
-    const threeDays = moment().subtract(2,'day').format('L');
-    const fourDays = moment().subtract(3,'day').format('L');
-    const fiveDays = moment().subtract(4,'day').format('L')
-    const sixDays = moment().subtract(5,'day').format('L')
-    const sevenDays = moment().subtract(6,'day').format('L')
-    console.log(today)
-    return (
-      <div>
-        <Line
-          data={state}
-          options={{
-            title:{
-              display:true,
-              text:'Average Rainfall per month',
-              fontSize:20
-            },
-            legend:{
-              display:true,
-              position:'right'
-            }
-          }}
-        />
-      </div>
-    );
-  }
-}
+export default RevenueChart;
--- client/src/components/RevenueList/index.js
+++ client/src/components/RevenueList/index.js
@@ -5,31 +5,74 @@ import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import CustomSearch from '../CustomSearch';
 import InvoiceModal from '../InvoiceModal';
+import RevenueChart from '../RevenueChart'
 import { Link } from "react-router-dom";
 
 
 function RevenueList(){
+  const [weekInv, setWeekInv] = useState([]);
   const [tableHistory, setTableHistory] = useState([]);
-  const [todayTrans, setTodayTrans] = useState([])
+  const [todayTrans, setTodayTrans] = useState([]);
   const [selected, setSelected] = useState({
     invoices:[],
     date: ""
+  });
+
+  const [total, setTotal]= useState({
+    today: 0,
+    yesterday: 0,
+    threeDays: 0,
+    fourDays: 0,
+    fiveDays: 0,
+    sixDays: 0,
+    sevenDays: 0
   })
 
-  const today = moment().format('L').toString()
-  const yesterday = moment().subtract(1, 'day').format('L')
+ 
+  const today = moment().format('L').toString();
+  const yesterday = moment().subtract(1, 'day').format('L');
+  const threeDays = moment().subtract(2,'day').format('L');
+  const fourDays = moment().subtract(3,'day').format('L');
+  const fiveDays = moment().subtract(4,'day').format('L')
+  const sixDays = moment().subtract(5,'day').format('L')
+  const sevenDays = moment().subtract(6,'day').format('L')
   const week = moment().subtract(7, 'day').format('L')
+
+  const daysOfWeek = [today, yesterday, threeDays, fourDays, fiveDays, sixDays, sevenDays]
   
   function getTableHistory() {  
     API.getTableHistory()
       .then(results => {
-        console.log(results.data)
         setTableHistory(results.data)
-        console.log(today)
         let transaction = results.data.filter(receipt => receipt.date === today)
         setSelected({ invoices: transaction , date: today})
         setTodayTrans(transaction)
-    })
+        results.data.forEach(invoice =>{
+          if(invoice.date === today){
+            let todayTotal = total.today + invoice.total_price
+            setTotal({today: todayTotal})
+          } else if(invoice.date === yesterday){
+            let yesterdayTotal = total.yesterday + invoice.total_price
+            setTotal({yesterday: yesterdayTotal})
+          } else if(invoice.date === threeDays){
+            let threeDayTotal = total.threeDays + invoice.total_price
+            setTotal({threeDays: threeDayTotal})
+          } else if(invoice.date === fourDays){
+            let fourDayTotal = total.fourDays + invoice.total_price
+            setTotal({fourDays: fourDayTotal})
+          } else if(invoice.date === fiveDays){
+            let fiveDayTotal = total.fiveDays + invoice.total_price
+            setTotal({fiveDays: fiveDayTotal})
+          } else if(invoice.date === sixDays){
+            let sixDayTotal = total.sixDays + invoice.total_price
+            setTotal({sixDays: sixDayTotal})
+          } else if(invoice.date === sevenDays){
+            let sevenDayTotal = total.sevenDays + invoice.total_price
+            setTotal({sevenDays: sevenDayTotal})
+          }
+        })
+      }
+    )
   };
 
   function getToday(){
@@ -66,12 +109,8 @@ function RevenueList(){
   useEffect(() => {
     getTableHistory()
   }, []);
-
-//   console.log(today)
-//   console.log(yesterday)
-  console.log(tableHistory)
-//   console.log(todayTrans)
-  console.log(selected)
+  
+  console.log(total)
   return(
     <div>
       <div>
@@ -79,6 +118,7 @@ function RevenueList(){
         <button variant="primary" style={{width: "100px", margin: "1%"}} className="btn btn-primary" onClick={getYesterday}>Yesterday</button>
         <button variant="primary" style={{width: "100px", margin: "1%"}} className="btn btn-primary" onClick={getWeek}>Week</button>
         <CustomSearch userSearch={userSearch} />
+        <RevenueChart daysOfWeek={daysOfWeek} total={total} />
         <Link to="/Account">
           <button className="login-button" style={{margin: "1%", marginLeft: "3%"}}>Account</button>
         </Link>
