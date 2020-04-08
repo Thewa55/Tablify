574c4705846605bb2dc9543556707842b585e481
Kenny Lam <kinwai.lam730@gmail.com>
2020-04-08 14:39:18 -0400

    Finished kitchen page

--- client/src/pages/Kitchen.js
+++ client/src/pages/Kitchen.js
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../style.css";
 import KitchenCard from "../components/KitchenCard";
 import Navbar from "../components/Navbar";
-
-const styles = {
-  sectionStyles: {
-    background: "orange"
-  }
-};
+import API from '../utils/API'
 
 function Kitchen() {
+
+  const [tables, setTables] = useState([])
+
+  function getSavedTable () {
+    API.getTables().then(res => {
+      setTables(res.data )
+    })
+  };
+
+  const cooked = (id) => {
+    console.log(id)
+    const orderFinished =  tables.forEach(table => table._id !== id)
+    setTables(orderFinished)
+  }
+
+
+  useEffect(() => {
+    getSavedTable()
+  }, [])
+
+  console.log(tables)
   return (
     <div>
       <div class="col-md-2">
         <Navbar/>
-        <KitchenCard/>
-        <KitchenCard/>
+        {tables ? (
+          <>
+            {tables.map(table =>
+              table.order ? (
+                <KitchenCard table={table} cooked={cooked} />
+              ):(
+                <>
+                </>
+              ))}
+            </>) : (
+            <>
+            </>
+            )}
       </div>
     </div>
   );
