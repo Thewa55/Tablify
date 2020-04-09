dfa3fc8b0f0332082d0a22403cee1e412d6bd5c0
Kenny Lam <kinwai.lam730@gmail.com>
2020-04-08 20:32:21 -0400

    Trying some code

--- client/src/components/CustomSearch/index.js
+++ client/src/components/CustomSearch/index.js
@@ -2,13 +2,18 @@ import React, { useState } from 'react'
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
 import API from '../../utils/API'
+import moment from 'moment'
 // import EmployeeContext from '../../utils/EmployeeContext'
 
 function CustomSearch(props) {
     const [show, setShow] = useState(false); 
+    const [check, setCheck] = useState(true)
+    const [err, setError] = useState("")
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true)
+
+    const today = moment().format('L').toString();
 
     const [range, setRange] = useState({
       startDate: "",
@@ -18,11 +23,26 @@ function CustomSearch(props) {
     function handleInputChange(event){
         const { name, value } = event.target
         setRange({...range, [name]: value})
+        if(moment(range.endDate).isAfter(today, 'day')){
+          setError("Can't set end date after today")
+          setCheck(true)
+        }else if(range.startDate && range.endDate){
+          if(moment(range.endDate).isAfter(moment(range.startDate), 'day')){
+            setCheck(true)
+            setError("Can't set end date after the start date")
+          }
+        }else {setCheck(false)}
     }
 
     function handleSubmit (start, end){
+      if(moment(range.startDate).isAfter(today, 'day')){
+        setError("Can't set start date after today")
+        setCheck(true)
+        return
+      }else{
         props.userSearch(start, end);
         handleClose()
+      }
     }
 
 
@@ -60,12 +80,13 @@ function CustomSearch(props) {
                 />
               </div>
             </form>
+            <div>{err}</div>
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
               Cancel
             </Button>
-            <Button variant="primary" onClick={() => handleSubmit(range.startDate, range.endDate)}>
+            <Button variant="primary" disabled={check} onClick={() => handleSubmit(range.startDate, range.endDate)}>
               Submit
             </Button>
           </Modal.Footer>
--- client/src/components/RevenueChart/index.js
+++ client/src/components/RevenueChart/index.js
@@ -5,12 +5,22 @@ import {Line} from 'react-chartjs-2';
 
 
 function RevenueChart(props){
-  console.log("This props: ",props)
   const [show, setShow] = useState(false); 
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true)
 
+  // const [total, setTotal]= useState({
+  //   today: 0,
+  //   yesterday: 0,
+  //   threeDays: 0,
+  //   fourDays: 0,
+  //   fiveDays: 0,
+  //   sixDays: 0,
+  //   sevenDays: 0
+  // })
+  // const [today, setToday] = useState()
+  // const [yesterday, setYesterday] = useState()
   const [chart, setChart] = useState({
     labels: [],
     datasets: [{
@@ -24,10 +34,47 @@ function RevenueChart(props){
     ]
   })
 
+  
   function setPage() {
+    // let todayTotal = 0
+    // let yesterdayTotal = 0
+    // // let twoD = 0
+    // // let threeD = 0
+    // // let fourD = 0
+    // // let fiveD = 0
+    // // let sixD = 0
+    // // let sevenD = 0
+    
+    // props.tableHistory.forEach(invoice =>{
+    //   if(invoice.date === props.daysOfWeek[0]){
+    //     // console.log("Invoice date", invoice.date, " today's date", props.daysOfWeek[0])
+    //     todayTotal = todayTotal + invoice.total_price
+    //     setTotal({...total, today: todayTotal})
+    //   } else if(invoice.date === props.daysOfWeek[1]){
+    //     console.log("Invoice date", invoice.date, " yesterday's date", props.daysOfWeek[1])
+    //     yesterdayTotal = yesterdayTotal + invoice.total_price
+    //     console.log(yesterdayTotal, " Yesterday's total")
+    //     setTotal({...total, yesterday: yesterdayTotal})
+    //   } else if(invoice.date === props.daysOfWeek[0]){
+    //     let threeDayTotal = total.threeDays + invoice.total_price
+    //     setTotal({threeDays: props.daysOfWeek[0]})
+    //   } else if(invoice.date === props.daysOfWeek[0]){
+    //     let fourDayTotal = total.fourDays + invoice.total_price
+    //     setTotal({fourDays: fourDayTotal})
+    //   } else if(invoice.date === props.daysOfWeek[0]){
+    //     let fiveDayTotal = total.fiveDays + invoice.total_price
+    //     setTotal({fiveDays: fiveDayTotal})
+    //   } else if(invoice.date === props.daysOfWeek[0]){
+    //     let sixDayTotal = total.sixDays + invoice.total_price
+    //     setTotal({sixDays: sixDayTotal})
+    //   } else if(invoice.date === props.daysOfWeek[0]){
+    //     let sevenDayTotal = total.sevenDays + invoice.total_price
+    //     setTotal({sevenDays: sevenDayTotal})
+    //   }
+    // })
     setChart({...chart, labels: props.daysOfWeek, 
-      datasets:[{data: [props.total.today, props.total.yesterday, props.total.threeDays, props.total.fourDays, props.total.fiveDays, props.total.sixDays, props.total.sevenDays]}]
-      // datasets: {data: [props.total.today, props.total.yesterday, props.total.threeDays, props.total.fourDays, props.total.fiveDays, props.total.sixDays, props.total.sevenDays]}
+      // datasets:[{data: [props.total.today, props.total.yesterday, props.total.threeDays, props.total.fourDays, props.total.fiveDays, props.total.sixDays, props.total.sevenDays]}]
+      // datasets:[{data: [total.today, total.yesterday, total.threeDays, total.fourDays, total.fiveDays, total.sixDays, total.sevenDays]}]
     })
     handleShow()
   }
